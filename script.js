d1b3c4455b502c3e8456463927376119698fdb41
Panther2113 <aburtoa2113@gmail.com>
2021-05-15 22:57:57 -0500

    Initial commit contain js css html

---
+++ script.js
@@ -0,0 +1,95 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Create a array for upper case alpha, lower case alpha, special character, and number
+
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "<", ">","/", "~", "=", "+"]
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y", "Z"]
+var numericalCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+// makes password to #password input form.
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+// draws a password content from the information retrieved by the efunctions
+  passwordText.textContent = password;
+
+}
+
+
+
+// adds event listener to generate button.
+generateBtn.addEventListener("click", writePassword);
+
+
+//Defines a item as a value selected from specific array
+function random(arr) {
+  let randomindex = Math.floor(Math.random()*arr.length)
+  return arr[randomindex]
+} 
+
+function passwordOptions() {
+  //Prompts for password length
+  let passwordLength = parseInt(prompt ("How many characters would you like your password ? (has to be between 8 and 128 characters)"));
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert ("Passwprd length must be between 8 to 128 Characters.")
+    return passwordOptions()
+  }
+    
+ 
+  //Prompt user for an upper alpha
+ let uppers = confirm("Would you like to include Uppercase Letters?")
+ let lowers = confirm("Would you like to include Lowercase Letters?")
+ let numbers = confirm("Would you like to include Numbers?")
+ let specchar = confirm("Would you like to include Special Characters?")
+  
+  let options = {
+    passwordLength, uppers, lowers, numbers, specchar
+  }
+  return options
+}
+//These define the names of functions and objects in the code.
+
+function generatePassword() {
+  let options = passwordOptions();
+  let concats = []
+  let guaranted = []
+  let finalpassword = []
+
+
+
+if (options.numbers) {
+  concats = concats.concat(numericalCharacter)
+  guaranted.push(random(numericalCharacter))
+}
+
+if (options.specchar) {
+  concats =[...concats, ...specialCharacter]
+  guaranted.push(random(specialCharacter))
+  }
+
+if (options.uppers) {
+    concats = concats.concat(upperLetters)
+    guaranted.push(random(upperLetters))
+  }
+
+if (options.lowers) {
+  concats = concats.concat(lowerLetters)
+  guaranted.push(random(lowerLetters))
+}
+
+console.log(concats)
+console.log(guaranted)
+
+
+for(let i = 0; i < options.passwordLength; i++) {
+finalpassword.push(random(concats))
+}
+for(let i = 0; i < guaranted.length; i++) {
+  finalpassword[i] = guaranted[i]
+  }
+return finalpassword.join("")
+}
+
